98ebc07e25d94a92bce205e7760951ea17c82bc3
Rudi Giesler <rudi@praekelt.com>
2014-12-23 16:42:45 +0200

    Added all the required fields to app settings

--- app_settings.py
+++ app_settings.py
@@ -2,3 +2,8 @@ import settings
 
 API_URL = getattr(settings, 'API_URL', 'go.vumi.org/api/v1/go')
 AUTH_TOKEN = getattr(settings, 'AUTH_TOKEN', None)
+FIELDS = getattr(settings, 'FIELDS', [
+    'name', 'surname', 'email_address', 'dob', 'created_at', 'twitter_handle',
+    'facebook_id', 'bbm_pin', 'gtalk_id', 'mxit_id', 'wechat_id', 'msisdn'])
+DICT_FIELDS = getattr(settings, 'DICT_FIELDS', ['extra', 'subscription'])
+LIST_FIELDS = getattr(settings, 'LIST_FIELDS', ['groups'])
